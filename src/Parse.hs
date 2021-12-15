b769679efaf15c6c0b33745d59411e28a940bdc4
Yingzhe Wan <627311198@qq.com>
2021-12-15 01:21:21 +0000

    add Maybe to parseMovie

--- src/Parse.hs
+++ src/Parse.hs
@@ -19,17 +19,19 @@ bsToStr = Prelude.map (chr . fromEnum) . unpack
 getTextOfANode :: Node -> String
 getTextOfANode node = bsToStr t where Text t = Prelude.head $ contents node
 
-getTextOfChildren :: [Node] -> String -> String
+getTextOfChildren :: [Node] -> String -> Maybe String
+getTextOfChildren [] _ = Nothing
 getTextOfChildren (thisChild : nextChildren) childName 
-    | bsToStr (Xeno.DOM.name thisChild) == childName = getTextOfANode thisChild
+    | bsToStr (Xeno.DOM.name thisChild) == childName = Just (getTextOfANode thisChild)
     | otherwise = getTextOfChildren nextChildren childName
 
 parseMovie :: Node -> Movie
 parseMovie node =
-    let mtitle = getTextOfChildren (children node) "title"
-        mcc = (getTextOfChildren (children node) "cc") == "Y" 
-        mrating = getTextOfChildren (children node) "rating"
-    in Movie {title = mtitle, cc = mcc, rating = mrating, underwriter = Nothing}
+    let Just mtitle = getTextOfChildren (children node) "title"
+        mcc = (getTextOfChildren (children node) "cc") == Just "Y" 
+        Just mrating = getTextOfChildren (children node) "rating"
+        munderwriter = getTextOfChildren (children node) "underwriter"
+    in Movie {title = mtitle, cc = mcc, rating = mrating, underwriter = munderwriter}
  
 
 xml2RowNodes :: L8.ByteString -> [Node]
