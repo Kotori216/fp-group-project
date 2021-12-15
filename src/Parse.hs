7cbff22fdad951cb7347825fc1598b7cfbcbdfb9
Yingzhe Wan <627311198@qq.com>
2021-12-15 07:07:44 +0000

    add Haddock-style documentation to Parse.hs

--- src/Parse.hs
+++ src/Parse.hs
@@ -11,18 +11,22 @@ import qualified Data.ByteString.Lazy.Char8 as L8
 import Control.Exception
 import Data.Char (chr)
 
+-- |The 'bsToStr' function converts a ByteString to a String.
 bsToStr :: ByteString -> String
 bsToStr = Prelude.map (chr . fromEnum) . unpack
 
+-- |The 'getTextOfANode' function takes a Node and returns the text of the Node
 getTextOfANode :: Node -> String
 getTextOfANode node = bsToStr t where Text t = Prelude.head $ contents node
 
+-- |The 'getTextOfChildren' function recursively finds the text of the child node which matches the child name
 getTextOfChildren :: [Node] -> String -> Maybe String
 getTextOfChildren [] _ = Nothing
 getTextOfChildren (thisChild : nextChildren) childName 
     | bsToStr (Xeno.DOM.name thisChild) == childName = Just (getTextOfANode thisChild)
     | otherwise = getTextOfChildren nextChildren childName
 
+-- |The 'parseRow' function converts a Node to a Row
 parseRow :: Node -> Row
 parseRow node =
     let rid = bsToStr $ snd $ Prelude.head $ attributes $ node
@@ -48,11 +52,13 @@ parseRow node =
         address_ = raddress
     }
 
+-- |The 'parseRows' function parses xml to Rows
 parseRows :: L8.ByteString -> Rows
 parseRows xml = 
     let rowList = Prelude.map parseRow $ xml2RowNodes xml
     in Rows {rows = rowList}
 
+-- |The 'xml2RowNodes' function parses xml to Node list
 xml2RowNodes :: L8.ByteString -> [Node]
 xml2RowNodes xml = case parse (L8.toStrict xml) of
     Left err -> throw err
