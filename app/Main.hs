cc5a61c213115840e405d68af7c6ad0410624065
Yingzhe Wan <627311198@qq.com>
2021-12-15 01:00:32 +0000

    add parseMovie

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import Fetch
+import Parse
 import Xeno.DOM
 import Data.ByteString 
 import qualified Data.ByteString.Lazy.Char8 as L8
@@ -10,5 +11,20 @@ main = do
     let url = "https://data.cityofchicago.org/api/views/7piw-z6r6/rows.xml"
     print "Downloading..."
     xml <- download url
-    let (Right result) = parse (L8.toStrict xml)
-    print $ Prelude.head $ children $ Prelude.head $ children result
+    print "=========Node"
+    let node = Prelude.head $ xml2RowNodes xml
+    print node
+    print "=========name"
+    print $ name node
+    print "=========attributes"
+    print $ attributes node
+    print "=========contents"
+    print $ contents node
+    print "=========children"
+    print $ children node
+    print "=========first child contents text"
+    let Text t = Prelude.head $ contents $ Prelude.head $ children node
+    print t
+    print "=========first movie"
+    print $ parseMovie node
+
