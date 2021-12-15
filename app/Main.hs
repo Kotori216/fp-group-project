1977a8c063d70e800ec1ef3708c08112a173c16f
Qifei Wang <94302872+maggiewang027@users.noreply.github.com>
2021-12-15 19:14:06 +0000

    Update Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -11,42 +11,42 @@ import qualified Data.ByteString.Lazy.Char8 as L8
 main :: IO ()
 main = do
     let url = "https://data.cityofchicago.org/api/views/7piw-z6r6/rows.xml"
-    print "Downloading..."
+    putStrLn "Downloading..."
     xml <- download url
-    print "Parsing..."
+    putStrLn "Parsing..."
     let rows_ = rows $ parseRows xml
     conn <- initialiseDB
-    print "Saving on DB..."
+    putStrLn "Saving on DB..."
     saveRows conn rows_
-    print "Saved!"
-    print "The XML is now ready for query processing"
-    print "----------------------------------------------------"
-    print "  Welcome to the Movie info app                     "
-    print "  Plsease choose an query option below:             "
-    print "  (1) Query by entering the park name               "
-    print "  (2) Query by entering the movie name              "
-    print "  (3) Query by entering both the park & movie name  "
-    print "  (4) Quit                                          "
-    print "----------------------------------------------------"
+    putStrLn "Saved!"
+    putStrLn "The XML is now ready for query processing"
+    putStrLn "----------------------------------------------------"
+    putStrLn "  Welcome to the Movie info app                     "
+    putStrLn "  Plsease choose an query option below:             "
+    putStrLn "  (1) Query by entering the park name               "
+    putStrLn "  (2) Query by entering the movie name              "
+    putStrLn "  (3) Query by entering both the park & movie name  "
+    putStrLn "  (4) Quit                                          "
+    putStrLn "----------------------------------------------------"
     option <- readLn :: IO Int
     case option of
         1 -> do
             parkAllEvents <- queryParkAllEvents conn
             case parkAllEvents of
-                Nothing -> print "Could't find events for the given park name"
-            mapM_ print parkAllEvents
+                [] -> putStrLn "Could't find events for the given park name"
+                otherwise -> mapM_ print parkAllEvents
             main
         2 -> do
             movieAllEvents <- queryMovieAllEvents conn
             case movieAllEvents of
-                Nothing -> print "Could't find events for the given movie name"
-            mapM_ print movieAllEvents
+                [] -> putStrLn "Could't find events for the given movie name"
+                otherwise -> mapM_ print movieAllEvents
             main
         3 -> do
             parkMovieAllEvents <- queryParkMovieAllEvents conn
             case parkMovieAllEvents of
-                Nothing -> print "Could't find events for the given park & movie name"
-            mapM_ print parkMovieAllEvents
+                [] -> putStrLn "Could't find events for the given park & movie name"
+                otherwise -> mapM_ print parkMovieAllEvents
             main
-        4 -> print "Hope you've enjoyed using the app!"
-        otherwise -> print "Invalid option"
+        4 -> putStrLn "Hope you've enjoyed using the app!"
+        otherwise -> putStrLn "Invalid option"
